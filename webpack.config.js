e3cdc1097919601b56245f3a0ab75282b85c5a73
Jonathan Gill <mrjonathangill@gmail.com>
2016-11-10 08:44:12 +0100

    All this documentation, wow

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: ['whatwg-fetch','./client/app.js'],
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.*css$/,
+        loader: ExtractTextPlugin.extract('css!sass', { publicPath: '../'})
+      }
+    ]
+  },
+  devtool: 'source-map',
+  sassLoader: {
+   includePaths: [path.resolve(__dirname, "client/lib/sass")]
+ },
+ plugins: [
+   new ExtractTextPlugin('style.bundle.css', {
+       allChunks: true
+   })
+ ],
+  output: {
+    path: './build',
+    filename: 'app.bundle.js'
+  }
+}
